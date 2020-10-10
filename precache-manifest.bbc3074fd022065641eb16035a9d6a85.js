204118254588ac01f354cde100674473806c07da
Jonathan-Odia Osahon Emmanuel <emmyodia@gmail.com>
2020-10-10 02:24:26 +0100

    Updates

---
+++ precache-manifest.bbc3074fd022065641eb16035a9d6a85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20481a70f5e554e0a24e10f976da88df",
+    "url": "/news-react-app/index.html"
+  },
+  {
+    "revision": "59f3b5548a721f3e73f0",
+    "url": "/news-react-app/static/css/main.c6e0c7a5.chunk.css"
+  },
+  {
+    "revision": "82ca86b2796a86214a8d",
+    "url": "/news-react-app/static/js/2.9597891f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/news-react-app/static/js/2.9597891f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59f3b5548a721f3e73f0",
+    "url": "/news-react-app/static/js/main.11d91c68.chunk.js"
+  },
+  {
+    "revision": "6a8a65df5550a13845d6",
+    "url": "/news-react-app/static/js/runtime-main.1a16f03a.js"
+  }
+]);
